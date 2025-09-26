09624f17fb94679c5ee11fd48fd890872b5952d6
KoraDraw <korinachernysheva@mail.ru>
2025-09-26 18:13:27 +0300

    change  title

--- src/widgets/LayoutHeader/Header.tsx
+++ src/widgets/LayoutHeader/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Header: React.FC = () => {
   return (
     <header style={{ padding: 16, backgroundColor: "#282c34", color: "white" }}>
-      <h1>Моё приложение</h1>
+      <h1>Приложение для постов</h1>
     </header>
   );
 };
